882032c1a314417c7eec3f05feb1b13487efd123
Vikas Seth <seth.vikas@icloud.com>
2023-07-22 16:44:16 -0400

    Some refinements in HTML

--- basic-flask-app/app.py
+++ basic-flask-app/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-# Flask App
+# Define Flask App
 app = Flask(__name__)
 
 # Secret Key
 app.config['SECRET_KEY'] = 'my-secret-key'
 
-@app.route('/')
-def index():
 
+@app.route('/')
+def index1():
     return render_template('index.html')
 
 if __name__ == '__main__':
